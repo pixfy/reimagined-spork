a9e278e6d5c730bf00dcaa8fbc7ea739f7e53d51
Johnny <johkker@gmail.com>
2023-01-24 15:06:52 -0300

    feat: added fetch button on web to get info from backend. modified service on api to send correct data

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,14 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import users from '../data/users.js';
 
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
 
-app.post("/api/users", (req, res, next) => {
-  res.status(200).send([]);
+app.get('/api/users', (req, res, next) => {
+  res.status(200).send(users);
 });
 
 app.listen(PORT, () => console.log(`Running app in port: ${PORT}`));
--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,54 @@
-import "./App.css";
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const api = axios.create({
+  baseURL: 'http://localhost:4000/api',
+});
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [heroes, setHeroes] = useState([]);
+  const [usedHeroes, setUsedHeroes] = useState([]);
+
+  const getHeroes = async () => {
+    await api.get('/users').then((res) => {
+      console.log('making request');
+      setHeroes(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getHeroes();
+  }, []);
+
+  const addHero = () => {
+    const nextHero = heroes[counter];
+    setUsedHeroes([...usedHeroes, nextHero]);
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
-      <h2>reimagined-spork</h2>
+      <h2>Heroes</h2>
+      <button
+        onClick={() => {
+          addHero();
+        }}
+        disabled={counter >= heroes.length}
+      >
+        Fetch
+      </button>
+      <div className="heroes-container">
+        {usedHeroes.map((hero) => {
+          return (
+            <div key={`hero ${hero.id}`}>
+              <img src={hero.image} alt={hero.name} className="hero-image" />
+              <p>{hero.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
